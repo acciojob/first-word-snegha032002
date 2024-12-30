04adbd4423a1a3afbc4880d085cea644a1f145f7
Snegha R <sneghakumar0315@gmail.com>
2024-12-30 17:44:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function firstWord(s) {
   // your code here
+	 s = s.trim();
+
+
+    const spaceIndex = s.indexOf(' ');
+
+    if (spaceIndex === -1) {
+        return s; 
+    } else {
+        return s.slice(0, spaceIndex);
+    }
+}
+
+// Example usage
+console.log(firstWord('see and stop'));  
+            
+
 }
 
 // Do not change the code below
